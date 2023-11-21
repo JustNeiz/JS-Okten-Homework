9a88cc8bfe8da34c030251341e130acdb0418515
justneiz <timur0953017585@gmail.com>
2023-11-21 11:04:40 +0200

    hw1 pars age to int add tasks in comments

--- Homework1/main.js
+++ Homework1/main.js
@@ -1,3 +1,17 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в
+// одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям,
+// По-Батькові та роками. та вивести в консоль
+
+
 let sA = 'hello';
 let sB = 'owu';
 let sC = 'com';
@@ -43,4 +57,6 @@ console.log(typeof c, c)
 let promName = prompt('Ваше імʼя');
 let promSurname = prompt('Ваше прізвище');
 let promAge = prompt('Ваш вік');
-console.log(`${promSurname} ${promName}, ${promAge} роки.`)
+promAge = parseInt(promAge);
+console.log(`${promSurname} ${promName}, ${promAge} роки.`);
+console.log(typeof promAge, promAge);
